9d9bbc81968a97bd0b0f97d48e84acbf4710ae27
Cindy Sullivan <31637301+cindy-pi@users.noreply.github.com>
2022-12-04 19:37:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import wifi
 
 ## Import WiFi, expects your SSID and Password to be in secrets.py file
 wifi.Connect()
+
+## Starts you Web Service, it will display the index.html file.
+webServer.Start()
